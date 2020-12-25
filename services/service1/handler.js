3629626904cc911edbdf1f1d6a5f3ae811a7d06c
Diegol <diego.lewin@gmail.com>
2020-12-25 18:50:47 +1300

    update package

--- services/service1/handler.js
+++ services/service1/handler.js
@@ -4,6 +4,6 @@ import lib from "../../libs";
 export async function main(event, context) {
   return {
     statusCode: 200,
-    body: `Hello World  ! Via ${sample()} and ${lib()}.`,
+    body: `Hello World   ! Via ${sample()} and ${lib()}.`,
   };
 }
